e52570dec8a79a59c27ccb254aa1472104f8ba49
Kauan Rodrigues <kauanrdx145@gmail.com>
2023-01-25 14:46:56 -0300

    link do deploy no arquivo instance.js

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://tasks-management-back-end.cyclic.app/',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://tasks-management-back-end.cyclic.app/',
+    // baseURL: 'http://localhost:3000',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
 })
